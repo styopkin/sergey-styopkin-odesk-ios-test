991d43eabf8c63697b73f1a02168a746592e4d41
Nabi Makhmudov <nebary@yandex.ru>
2014-03-04 15:29:59 +0400

    Initial Commit

---
+++ BuggyProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BuggyProject
+//
+//  Created by Nabi Makhmudov on 3/4/14.
+//  Copyright (c) 2014 oDesk Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
